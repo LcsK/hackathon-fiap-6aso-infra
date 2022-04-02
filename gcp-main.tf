2cfc81f2b37ea7d4c7cc0208e64121fe0aca4b68
lcsk <lucasaferrlopes@gmail.com>
2022-04-02 14:29:37 -0300

    fix dump bug

--- gcp-main.tf
+++ gcp-main.tf
@@ -11,7 +11,6 @@ resource "google_sql_database_instance" "main" {
   name             = "main-instance"
   database_version = "MYSQL_5_7"
   region           = "us-central1"
-  dump_file_path = "Playlist.sql"
   settings {
     # Second-generation instance tiers are based on the machine
     # type. See argument reference below.
