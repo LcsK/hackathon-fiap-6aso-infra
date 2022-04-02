b00235d4446d67bbecfc90622453315ed3cfd0a9
lcsk <lucasaferrlopes@gmail.com>
2022-04-02 14:21:23 -0300

    add cloud-sql to terraform

--- gcp-main.tf
+++ gcp-main.tf
@@ -6,3 +6,44 @@ resource "google_artifact_registry_repository" "my-repo" {
   description = "Imagens Docker"
   format = "DOCKER"
 }
+
+resource "google_sql_database_instance" "main" {
+  name             = "main-instance"
+  database_version = "MYSQL_5_7"
+  region           = "us-central1"
+  dump_file_path = "Playlist.sql"
+  settings {
+    # Second-generation instance tiers are based on the machine
+    # type. See argument reference below.
+    tier = "db-f1-micro"
+  }
+}
+
+variable "DATABASE_USER" {
+  type = string
+  default = ""
+}
+
+variable "DATABASE_PASSWORD" {
+  type = string
+  default = ""
+}
+
+variable "TFC_CONFIGURATION_VERSION_GIT_BRANCH" {
+  type = string
+  default = ""
+}
+
+resource "google_sql_database" “database” {
+	name = "main-database"
+	instance = "${google_sql_database_instance.main.name}"
+	charset = "utf8"
+	collation = "utf8_general_ci"
+}
+
+resource "google_sql_user" "users" {
+	name = var.DATABASE_USER
+	instance = "${google_sql_database_instance.main.name}"
+	host = "%"
+	password = var.DATABASE_PASSWORD
+}
